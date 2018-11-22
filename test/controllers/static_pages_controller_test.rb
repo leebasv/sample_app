5a2f054bda141ee769875f69c2abe4dfa2dcb165
leebasv <leebasv@gmail.com>
2018-11-22 10:32:57 +0700

    fix rubocop

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,5 @@
-require 'test_helper'
-
+require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   test "should get root" do
     get FILL_IN
     assert_response FILL_IN
