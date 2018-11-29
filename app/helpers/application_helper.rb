8148b893362d171a54cebb0949ba80d401e53205
leebasv <leebasv@gmail.com>
2018-11-29 16:30:45 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,5 @@ module ApplicationHelper
   def full_title page_title = ""
     base_title = t "layouts.application.base_title"
     page_title.blank? ? base_title : page_title + " | " + base_title
-    end
+  end
 end
