cd6e73b0fc4dcbe35fc6bde4191dca1972960d7c
leebasv <leebasv@gmail.com>
2018-12-05 11:09:18 +0700

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,22 @@
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../config/environment", __dir__)
+require_relative "../config/environment"
 require "rails/test_help"
-require "minitest/reporters"
-Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   fixtures :all
-  # Add more helper methods to be used by all tests here...
-  # Returns true if a test user is logged in.
   def is_logged_in?
     session[:user_id].present?
+  end
+
+  def log_in_as user
+    session[:user_id] = user.id
+  end
+
+  class ActionDispatch::IntegrationTest
+    def log_in_as user, password: "password", remember_me: "1"
+      post login_path, params: {session: {email: user.email,
+                                          password: password,
+                                          remember_me: remember_me}}
+    end
   end
 end
